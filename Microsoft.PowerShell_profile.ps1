84751a139b0d6ef0533779c8120337a5cfca0f84
Alex Li <li.alex100@gmail.com>
2018-05-08 14:40:45 +0800

    update wd

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,21 @@
-function  cdp {cd C:\source\pacific\src\}
-function  exp{explorer .}
-function  gs{git status}
-function vnc($ip) { 
-& "C:\Program Files\uvnc bvba\UltraVNC\vncviewer.exe" 192.168.1.$ip -password rocket -compresslevel 5 -quickoption 3 -autoscaling -quality 3
+
+
+function  wdfunction($location) {
+    switch ($location) {
+        p { cd C:\source\pacific\src\ }
+        Default {}
+    }
+}
+
+function  exp {explorer .}
+function  gs {git status}
+function vnc($ip) {
+    & "C:\Program Files\uvnc bvba\UltraVNC\vncviewer.exe" 192.168.1.$ip -password rocket -compresslevel 5 -quickoption 3 -autoscaling -quality 3
 }
 
 
 
-Set-Alias wdp cdp
+Set-Alias wd wdfunction
 
 Set-Alias c cls
 Set-Alias l ls
