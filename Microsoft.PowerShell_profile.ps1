bdf1d766981a5166b40142de9cff3e51765f293f
Alex Li <li.alex100@gmail.com>
2018-05-08 12:23:52 +0800

    fix bug

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,15 @@
 function  cdp {cd C:\source\pacific\src\}
+function  exp{explorer .}
+function  gs{git status}
 function vnc($ip) { 
 & "C:\Program Files\uvnc bvba\UltraVNC\vncviewer.exe" 192.168.1.$ip -password rocket -compresslevel 5 -quickoption 3 -autoscaling -quality 3
 }
+
+
+
 Set-Alias wdp cdp
 
 Set-Alias c cls
 Set-Alias l ls
-Set-Alias o explorer .
-Set-Alias gst git status 
+Set-Alias o exp
+Set-Alias gst gs
